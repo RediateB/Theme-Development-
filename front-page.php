0586ca25f26a721232fa253263b6be77eff3a28a
zekariyas-alemu <zekialemu3@gmail.com>
2022-01-10 12:28:10 +0300

    header and footer

---
+++ front-page.php
@@ -0,0 +1,47 @@
+<?php
+
+get_header();
+?>
+<header style="background:url(https://images.pexels.com/photos/733475/pexels-photo-733475.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940);    background-size: cover;
+background-position: center; height: 650px; border-top: 6px solid #5385C1;"> 
+    <div class="container"> 
+        <div class="row"> 
+            <div class="col-md-12"> 
+                <div style="padding-top:250px;" class="te text-center text-white"> 
+                    <h1>Welcome to EDUCATION</h1>
+                    <p>A free theme for wordpress websites based on education</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section id="about-section"> 
+    <div class="container"> 
+        <div class="row"> 
+            <div class="col-md-6"> 
+                <div class="about-text border-right"> 
+                    <h1>ABOUT US</h1>
+                    <p class="">some image</p>
+                    <button class="btn btn-primary">More</button>
+                </div>
+            </div>
+            <div class="col-md-6"> 
+                <div class="about-text border-right"> 
+                    <h1>ABOUT US</h1>
+                    <p>about us text</p>
+                    <button class="btn btn-primary">More</button>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<?php
+
+get_footer();
+
+?>
+
+
