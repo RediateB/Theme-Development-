d66df16f3c9f51f5a28f81a19e000623fcd1faec
zekariyas-alemu <zekialemu3@gmail.com>
2022-01-23 13:09:14 +0300

    day23 change 3

--- footer.php
+++ footer.php
@@ -28,5 +28,15 @@
 
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+  window.onscroll = function() {scrollFunction()}; 
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.querySelector(".navbar").style.backgroundColor = "#63ffc2";
+    } else {
+      document.querySelector(".navbar").style.backgroundColor = "transparent";
+    }
+  }
+</script>
 </body>
 </html>
